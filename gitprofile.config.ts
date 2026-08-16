fb0d724129e253d5e3cae548e333c34825c784cb
smTanzim <121172341+TanzimJarif@users.noreply.github.com>
2026-08-16 19:48:01 +0100

    Update gitprofile configuration and details

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -7,7 +7,7 @@ const CONFIG = {
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * For example, if your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
   base: '/',
   projects: {
@@ -25,13 +25,13 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['TanzimJarif/relative-humidity-forecasting-mlops', 'TanzimJarif/propaganda-detection-llm'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['tanzimjarif/relative-humidity-forecasting-mlops'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
-      projects: [ 
+      projects: [
         {
           title: 'Project Name',
           description:
@@ -51,9 +51,9 @@ const CONFIG = {
       ],
     },
   },
-  seo: { title: 'Portfolio of Sarker MD Tanzim', description: '', imageURL: '' },
+  seo: { title: 'Portfolio of Sarker Tanzim', description: '', imageURL: '' },
   social: {
-    linkedin: 'sm-tanzim',
+    linkedin: 'sm-tanzim/',
     x: '',
     mastodon: '',
     researchGate: '',
@@ -61,85 +61,83 @@ const CONFIG = {
     instagram: '',
     reddit: '',
     threads: '',
-    youtube: '', 
+    youtube: '', // example: 'pewdiepie'
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: '',
-    dev: '',
-    stackoverflow: '',
+    medium: 'arifszn',
+    dev: 'arifszn',
+    stackoverflow: '', // example: '1/jeff-atwood'
     discord: '',
     telegram: '',
-    website: '',
+    website: 'https://www.arifszn.com',
     phone: '',
     email: '',
   },
   resume: {
     fileUrl:
-      'https://docs.google.com/document/d/1ApLSKTCaRBSbC1amEMt-Dypcdypc_HZt/edit?usp=sharing&ouid=103228794544147733958&rtpof=true&sd=true', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python',
     'SQL',
-    'R',
     'TensorFlow',
     'PyTorch',
     'AWS',
     'GCP',
-    'PowerBI',
-    'Docker',
-    'Matlabs',
-    'Databricks',
     'Spark',
+    'OpenAI',
     'Git',
+    'Docker',
+    'PowerBI',
   ],
   experiences: [
     {
-      company: 'YSD Consulting',
-      position: 'Data Scientist',
-      from: 'November 2021',
-      to: 'June 2025',
-      companyLink: 'https://www.ysdconsulting.co.uk/',
+      company: 'Company Name',
+      position: 'Position',
+      from: 'September 2021',
+      to: 'Present',
+      companyLink: 'https://example.com',
     },
     {
-      company: 'Service Engine',
-      position: 'Software Engineer',
-      from: 'November 2019',
+      company: 'Company Name',
+      position: 'Position',
+      from: 'July 2019',
       to: 'August 2021',
-      companyLink: '',
+      companyLink: 'https://example.com',
     },
   ],
   certifications: [
     {
-      name: 'Probability - The Science of Uncertainty and Data',
-      body: 'MITX',
-      year: 'September 2025',
-      link: '',
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com',
     },
   ],
   educations: [
     {
-      institution: 'University of Sussex',
-      degree: 'MSc',
-      from: '2023',
-      to: '2024',
+      institution: 'Institution Name',
+      degree: 'Degree',
+      from: '2015',
+      to: '2019',
     },
     {
-      institution: 'North South University',
-      degree: 'BSc in Computer Science and Engineering',
-      from: '2014',
-      to: '2019',
+      institution: 'Institution Name',
+      degree: 'Degree',
+      from: '2012',
+      to: '2014',
     },
   ],
   publications: [
     {
-      title: 'Analysis of spatial data and time series for predicting magnitude of seismic zones in Bangladesh',
+      title: 'Publication Title',
       conferenceName: '',
-      journalName: ' Artificial Intelligence and Algorithms in Intelligent Systems',
-      authors: '',
-      link: '',
+      journalName: 'Journal Name',
+      authors: 'John Doe, Jane Smith',
+      link: 'https://example.com',
       description:
-        '',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
     {
       title: 'Publication Title',
@@ -163,7 +161,7 @@ const CONFIG = {
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
-    defaultTheme: 'sunset',
+    defaultTheme: 'lofi',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -173,14 +171,47 @@ const CONFIG = {
     // using user system preferences, instead of the hardcoded defaultTheme
     respectPrefersColorScheme: false,
 
-    // Display the ring in the profile picture
+    // Display the ring in Profile picture
     displayAvatarRing: true,
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
+      'light',
       'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
       'sunset',
+      'caramellatte',
       'abyss',
+      'silk',
+      'procyon',
     ],
   },
 
